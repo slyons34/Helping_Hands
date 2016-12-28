5322a0b6e70284e43f8ee0c777ad3673b00cf62f
MrAndTonio <howard.antonio.m@gmail.com>
2016-12-28 12:48:52 -0500

    updated donations controler

---
+++ HelpingHands/HelpingHands/Controllers/DonationsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HelpingHands.Models;
+
+namespace HelpingHands.Controllers
+{
+    public class DonationsController : Controller
+    {
+        private HelpingHandsEntities db = new HelpingHandsEntities();
+
+        // GET: Donations
+        public ActionResult Index()
+        {
+            return View(db.Donations.ToList());
+        }
+
+        // GET: Donations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donation);
+        }
+
+        // GET: Donations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Donations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserName,Quantity,DateTime,Description")] Donation donation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donations.Add(donation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(donation);
+        }
+
+        // GET: Donations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donation);
+        }
+
+        // POST: Donations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserName,Quantity,DateTime,Description")] Donation donation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(donation);
+        }
+
+        // GET: Donations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donation);
+        }
+
+        // POST: Donations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Donation donation = db.Donations.Find(id);
+            db.Donations.Remove(donation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- HelpingHands/HelpingHands/Models/Donation.cs
+++ HelpingHands/HelpingHands/Models/Donation.cs
@@ -4,19 +4,27 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 
 namespace HelpingHands.Models
 {
     public class Donation
     {
+        public Donation()
+        {
+            if (HttpContext.Current.User.Identity.Name == null)
+                UserName = "anonymous";
+            else UserName = HttpContext.Current.User.Identity.Name;
+        }
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string UserName { get; set; }
-        public string Item { get; set; }
+        public Location Location { get; set; }
+        public Category Item { get; set; }
         public string Quantity { get; set; }
         public string DateTime { get; set; }
-        public string Location { get; set; }
+       
         public string Description { get; set; }        
     }
 }
