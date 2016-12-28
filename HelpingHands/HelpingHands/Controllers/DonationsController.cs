ece8a67ee26e9aa238dd2919ecfc57257cb645b2
MrAndTonio <howard.antonio.m@gmail.com>
2016-12-28 15:28:14 -0500

    ...

--- HelpingHands/HelpingHands/Controllers/DonationsController.cs
+++ HelpingHands/HelpingHands/Controllers/DonationsController.cs
@@ -46,7 +46,7 @@ namespace HelpingHands.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,UserName,Quantity,DateTime,Description")] Donation donation)
+        public ActionResult Create([Bind(Include = "Id,UserName,Location,Item,Description,Quantity,DateTime")] Donation donation)
         {
             if (ModelState.IsValid)
             {
