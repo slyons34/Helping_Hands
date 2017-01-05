5111b485c1a021e60aa009bd0ca6583c1a1469e4
slyons34 <slyons34@yahoo.com>
2017-01-05 12:38:52 -0500

    a

--- HelpingHands/HelpingHands/Global.asax.cs
+++ HelpingHands/HelpingHands/Global.asax.cs
@@ -14,7 +14,7 @@ namespace HelpingHands
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            //Migrations.InitializeDB.Initialize();
+            Migrations.InitializeDB.Initialize();
         }
     }
 }
